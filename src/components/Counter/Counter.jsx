e4eb87d576056b7e3cfb47133477c15e9f67762b
Philippe Ngom <philngom@gmail.com>
2022-05-11 14:38:56 -0700

    wrote reducer function

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,36 +1,73 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useReducer } from 'react'
 
 const pinkRGB = `rgb(236, 72, 153)`
+const initialState = {
+  count: 0,
+  color: `rgb(236, 72, 153)`,
+}
+
+const reducer = (state, action) => {
+  const checkColor = (type, count) => {
+    let color
+    if (type === 'increment' && count + 1 > 0) {
+      color = `rgb(52, 211, 153)`
+    } else if (type === 'decrement' && count - 1 < 0) {
+      color = `rgb(239, 68, 68)`
+    } else {
+      color = `rgb(236, 72, 153)`
+    }
+  }
+
+  switch (action.type) {
+    case 'increment':
+      return {
+        count: state.count + 1,
+        color: checkColor(action.type, state.count),
+      }
+    case 'decrement':
+      return {
+        count: state.count - 1,
+        color: checkColor(action.type, state.count),
+      }
+    case 'reset':
+      return {
+        count: 0,
+        color: checkColor(action.type, state.count),
+      }
+    default:
+      throw new Error()
+  }
+}
 
 export default function Counter() {
-  const [count, setCount] = useState(0)
-  const [currentColor, setCurrentColor] = useState(pinkRGB)
+  // const [count, setCount] = useState(0)
+  // const [currentColor, setCurrentColor] = useState(pinkRGB)
 
-  useEffect(() => {
-    if (count === 0) {
-      setCurrentColor(pinkRGB)
-    }
+  // useEffect(() => {
+  //   if (count === 0) {
+  //     setCurrentColor(pinkRGB)
+  //   }
 
-    if (count > 0) {
-      setCurrentColor(`rgb(52, 211, 153)`)
-    }
+  //   if (count > 0) {
+  //     setCurrentColor(`rgb(52, 211, 153)`)
+  //   }
 
-    if (count < 0) {
-      setCurrentColor(`rgb(239, 68, 68)`)
-    }
-  }, [count])
+  //   if (count < 0) {
+  //     setCurrentColor(`rgb(239, 68, 68)`)
+  //   }
+  // }, [count])
 
-  const increment = () => {
-    setCount((prevState) => prevState + 1)
-  }
+  // const increment = () => {
+  //   setCount((prevState) => prevState + 1)
+  // }
 
-  const decrement = () => {
-    setCount((prevState) => prevState - 1)
-  }
+  // const decrement = () => {
+  //   setCount((prevState) => prevState - 1)
+  // }
 
-  const reset = () => {
-    setCount(0)
-  }
+  // const reset = () => {
+  //   setCount(0)
+  // }
 
   return (
     <main className="bg-black bg-opacity-90 min-h-screen flex flex-col items-center justify-center text-4xl text-pink-500">
